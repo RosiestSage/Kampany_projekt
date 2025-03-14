9d97589caa6a13f47fcdf770bb9ec29e60bf96c8
Pali David <pali.david2007@gmail.com>
2025-03-14 22:36:23 +0100

    header már van valahogy + reszponzivitás

--- static/script/script.js
+++ static/script/script.js
@@ -1,48 +1,109 @@
 let navHeight = 20;
-//let fillingHeight = document.querySelector('nav').clientHeight +"px";
-//document.getElementById('filling').style.height = fillingHeight;
-
 let lastScrollTop = 0;
-let imgWidth = 34;
-
+let imgHeight = 0;
 //filling height nem mukszik
+let bodyWidth = document.querySelector('body').clientWidth;
+let kivonando = 0;
+let navKivonando =0;
 
+window.addEventListener("load", () =>{
+    bodyWidth = document.querySelector('body').clientWidth
+    
+    if (bodyWidth <= 450){
+       imgHeight = 60;
+       document.querySelector('.nav_img').style.width = imgHeight + "%";
+
+       //navHeight = 20;
+       //navKivonando = [0, 1];
+       kivonando =0; 
+    }
+    if (bodyWidth < 700 && bodyWidth > 450){
+        imgHeight = 40;
+        document.querySelector('.nav_img').style.width = imgHeight + "%";
+
+        //navHeight = 30;
+        //navKivonando = [10, 10];
+        kivonando = 40; 
+
+
+    }
+    if (bodyWidth < 1000 && bodyWidth > 600){
+        imgHeight = 50;
+        document.querySelector('.nav_img').style.width = imgHeight + "%";
+
+        //navHeight = 30;
+        //navKivonando = [0, 1];
+        kivonando = 50;
+
+    }
+    if (bodyWidth > 1000 && bodyWidth < 1500){
+        imgHeight = 45;
+        document.querySelector('.nav_img').style.width = imgHeight + "%";
+
+        //navHeight = 30;
+        //navKivonando = [0, 1];
+        kivonando = 50; 
+    }
+    if (bodyWidth > 1500){
+        imgHeight = 26;
+        document.querySelector('.nav_img').style.width = imgHeight + "%";
+
+        //navHeight = 30;
+        //navKivonando = [0, 1];
+        kivonando = 50; 
+    }
+    document.querySelector('nav').style.height = navHeight + "%";
+})
 
 //header
 document.addEventListener("scroll", (e) => {
     let st = window.pageYOffset;
     if (st < 500){
         if (st > lastScrollTop ){
-            if (navHeight > 10){
-                document.querySelector('nav').style.height = navHeight -1 + "%";
-                document.getElementById('nav_img').classList = ["collapse"];
-                document.getElementById('11a').innerText = "11.Arabok";
-                document.getElementById('11a').style.marginLeft = -50 +"%";
+            if (navHeight > 9){
+                document.querySelector('nav').style.height = navHeight - 1 + "%";
+                document.querySelector('.nav_img').classList =  ["nav_img collapse"];
+                setTimeout(function () {document.querySelector(".a11").innerText = "11.Arabok"}, 100)
+
+                document.querySelector('.a11').style.marginLeft = - kivonando +"%";
                 document.getElementById('arabok').classList = "vanish";
                 navHeight--;
             }
 
         } else if (st < lastScrollTop){
-            if (navHeight > 9 && navHeight < 20){
+            if (navHeight > 9 && navHeight  < 20){
+                console.log(navKivonando)
                 document.querySelector('nav').style.height = navHeight + 1 + "%";
-                document.getElementById('nav_img').style.width = imgWidth + 1 + "%";
-                document.getElementById('nav_img').classList = ["expand"];
+                document.querySelector('.nav_img').classList = ["nav_img expand"];
                 document.getElementById('arabok').classList = "appear";
-                document.getElementById('11a').innerText = "11.A";
-                document.getElementById('11a').style.marginLeft = 0;
-
-                console.log("szexbaszás")
+                document.querySelector('.a11').innerText = "11.A";
+                document.querySelector('.a11').style.marginLeft = 0;
                 navHeight++;
             }
         }
     }
 
+    console.log(navHeight)
     lastScrollTop = st <= 0 ? 0 : st;
 }, false);
 
+document.querySelector(".hamburgermenu").addEventListener("click", () =>{
+    document.querySelector(".telefon_menu").style.display = "flex";
+    document.querySelector(".telefon_menu").classList.add("menuopen");
+})
 
 
+document.querySelector(".xbutton").addEventListener("click", () =>{
+    document.querySelector(".telefon_menu").classList = "telefon_menu menuclose";
+    setTimeout(function (){
+        document.querySelector(".telefon_menu").style.opacity = 0;
+        document.querySelector(".telefon_menu").style.zIndex = 0;
+        document.querySelector(".telefon_menu").style.display = "none";
+        document.querySelector(".telefon_menu").classList = "telefon_menu";
+    }, 500)
+    
 
+})
 
 
 
