b8177b2f0963598af1bf113c1c216d7097d3775b
Akos K <akosjdlk.60@gmail.com>
2025-03-14 10:54:43 +0100

    .

--- webserver/main.py
+++ webserver/main.py
@@ -10,18 +10,23 @@ app = q.Quart(__name__)
 app.secret_key = os.getenv("COOKIE_SIGNING_KEY", token_urlsafe())
 
 async def check_login():
-    if session.get("login"):
-        return
 
-    elif request.headers.get("CF-Access-User-Email", "ThisWillNeverBeInAJedlikEmailAddress-2dbb50e7-5ab8-4d61-aeda-8e802a25316a") in os.getenv("ADMIN_EMAILS", "kocsis.akos@students.jedlik.eu;akosjdlk.60@gmail.com"):
+    if request.headers.get("Cf-Access-Authenticated-User-Email", "a@a.a").split("@")[0] in os.getenv("ADMIN_EMAILS", "kocsis.akos"):
         session["login"] = True
+        return
+    
+    session["login"] = False
 
     q.abort(401)
 
 @app.get("/admin")
 async def admin_area():
     await check_login()
-    return await q.render_template("admin"), 200
+    return await q.render_template("admin.html"), 200
+
+@app.get("/admin/test")
+async def test():
+    return q.jsonify(dict(request.headers._list)), 200
 
 @app.post("/create-checkout-session")
 async def create_checkout_session():
@@ -44,7 +49,7 @@ async def success():
     if id is None or len(id) < 10 or len(id) > 66:
         q.abort(q.Response("<h1>Bad Request</h1><p><code style=\"background-color: #f4f4f4; color: #d63384; font-family: 'Courier New', Courier, monospace; font-size: 0.9em; padding: 2px 4px; border-radius: 4px; border: 1px solid #ddd;\">id</code> query parameter missing or malformed.</p>", 400))
 
-    return await q.render_template("success.html", icon_url=q.url_for('static', filename="arabok_icon.png"))
+    return await q.render_template("success.html", icon_url=q.url_for('static', filename="images/arabok_icon.png"))
 
 @app.route("/fetch-invoice-url")
 async def fetch_invoice():
@@ -66,4 +71,10 @@ async def fetch_invoice():
 
 @app.errorhandler(401)
 async def unauthorized(error):
-    return await q.render_template("401"), 401
+    try:
+        return await q.render_template("401.html", email=request.headers.get("Cf-Access-Authenticated-User-Email", "N/A")), 401
+    except:
+        raise
+
+
+app.run("0.0.0.0", 8080)
