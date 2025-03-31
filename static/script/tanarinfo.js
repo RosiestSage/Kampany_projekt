850646aa0fa7892651e7f3b7048f99b6cfbef10c
Páli Dávid <pali.david@students.jedlik.eu>
2025-03-31 22:47:44 +0200

    támogató oldal közel kész

--- static/script/tanarinfo.js
+++ static/script/tanarinfo.js
@@ -1,14 +1,14 @@
 const data = [
     //Név;Tanári;Kiket tanít;Fun Fact;Jutalom1,Jutalom2;Kép src
-    'Somjai László;206;10.A,12.B,9.E,9.NY,12.E,11.A,11.B,11.C;Szeretem a futballt, utálom a lilákat. Csak a Fradi;Feleletmentesítő,Dogamentesítő;"../static/images/Somesz.webp"',
-    'Kottra Richárd;Iroda;11.A;Fradista;-;"../static/images/Kottra.webp"',
-    'Sándor László;206;12.A,11.A,12.C,9.C,13.A;Slovag;Feleletmentesítő,Dogamentesítő;"../static/images/Slo.webp"',
-    'Póda László;206;9.C,10.C,9.D,10.E,9.B;Idős;Feleletmentesítő,Dogamentesítő;"../static/images/Póda.webp"',
-    'Turbék Ádám;206;10.D,11.A,9.C,9.NY,11.B,10.C,11.D;Spanunk;Feleletmentesítő,Dogamentesítő;"../static/images/Turbék.webp"',
-    'Bognár Pál;206;12.C,13.A,11.A,;Pali;Feleletmentesítő,Dogamentesítő;"../static/images/Pali.webp"',
-    'Ágoston Anett;206;11.D,10.A,12.E,11.A,13.B;Sigma;Feleletmentesítő,Dogamentesítő;"../static/images/Ágoston.jpg"',
-    'Balogh Anna;206;11.A,12.D,9.B,11.E,13.A,12.C;Spanom Anna;Feleletmentesítő,Dogamentesítő;"../static/images/Balogh Anna.webp"',
-    'Bors Henriette;206;11.A,12.A,9.KNY,12.D,11.D,10.D,13.D,13.E;Chill guy;Feleletmentesítő,Dogamentesítő;"../static/images/Bors Henriette.webp"',
+    'Somjai László;206;10.A,12.B,9.E,9.NY,12.E,11.A,11.B,11.C;Szeretem a futballt, utálom a lilákat. Csak a Fradi;Feleletmentesítő,Dogamentesítő;"../static/images/Somjai László.jpg"',
+    'Kottra Richárd;Iroda;11.A; Szeretek főzni, nagyon jó pizzát sütök. Ha van egy kis szabadidőm makettezek, manó- és gombaházakat gyártok.;-;"../static/images/Kottra Richárd.jpg"',
+    'Sándor László;206;12.A,11.A,12.C,9.C,13.A;Slovag;Feleletmentesítő,Dogamentesítő;"../static/images/Sándor László.jpg"',
+    'Póda László;206;9.C,10.C,9.D,10.E,9.B;Idős;Feleletmentesítő,Dogamentesítő;"../static/images/Póda László.jpg"',
+    'Turbék Ádám;206;10.D,11.A,9.C,9.NY,11.B,10.C,11.D;Spanunk;Feleletmentesítő,Dogamentesítő;"../static/images/Turbék Ádám.jpg"',
+    'Bognár Pál;206;12.C,13.A,11.A,;Pali;Feleletmentesítő,Dogamentesítő;"../static/images/Bognár Pál.jpg"',
+    'Ágoston Anett;206;11.D,10.A,12.E,11.A,13.B;Sigma;Feleletmentesítő,Dogamentesítő;"../static/images/Ágoston Anett.jpg"',
+    'Balogh Anna;206;11.A,12.D,9.B,11.E,13.A,12.C;Spanom Anna;Feleletmentesítő,Dogamentesítő;"../static/images/Balogh Anna.jpg"',
+    'Bors Henriette;206;11.A,12.A,9.KNY,12.D,11.D,10.D,13.D,13.E;Chill guy;Feleletmentesítő,Dogamentesítő;"../static/images/Bors Henriette.jpg"',
 ]
 
 export default data;
