80be566407055543b5af6cac8782b702ac86885d
Páli Dávid <pali.david@students.jedlik.eu>
2025-04-15 22:58:33 +0200

    Simítás

--- static/script/info.js
+++ static/script/info.js
@@ -4,12 +4,12 @@ const tanardata = [
     'Kottra Richárd;Iroda;11.A; Szeretek főzni, nagyon jó pizzát sütök. Ha van egy kis szabadidőm makettezek, manó- és gombaházakat gyártok.;Ölelés;"../static/images/Kottra Richárd.jpg"',
     'Sándor László;304;12.A,11.A,12.C,9.C,13.A;"Ne vitatkozz hülyékkel - Geszti Péter";Ölelés, Játékóra;"../static/images/Sándor László.jpg"',
     'Póda László;107;9.C,10.C,9.D,10.E,9.B;-;Feleletmentesítő;"../static/images/Póda László.jpg"',
-    'Turbék Ádám;B;10.D,11.A,9.C,9.NY,11.B,10.C,11.D;???;Ingyen szett, játékóra;"../static/images/Turbék Ádám.jpg"',
+    'Turbék Ádám;B;10.D,11.A,9.C,9.NY,11.B,10.C,11.D;-;Ingyen szett, játékóra;"../static/images/Turbék Ádám.jpg"',
     'Bognár Pál;101;12.C,13.A,11.A,;A demokrácia jó dolog, de a diktatúra működik;Feketepont törlése;"../static/images/Bognár Pál.jpg"',
     'Ágoston Anett;107;11.D,10.A,12.E,11.A,13.B;Vitathatatlan tény, hogy a kutyáktól mindig minden jobb lesz.;Feleletmentesítő;"../static/images/Ágoston Anett.jpg"',
-    'Balogh Anna;10;11.A,12.D,9.B,11.E,13.A,12.C;????;Feleletmentesítő,Dogahalasztó,Plusz;"../static/images/Balogh Anna.jpg"',
-    'Bors Henriette;211;11.A,12.A,9.KNY,12.D,11.D,10.D,13.D,13.E;????;Feleletmentesítő;"../static/images/Bors Henriette.jpg"',
-    'Csókáné Judit;??;9.D,12.D,13.A,10.D,13.E,12.A;?????;Feleletmentesítő;"../static/images/Csókáné Judit.jpg"',
+    'Balogh Anna;10;11.A,12.D,9.B,11.E,13.A,12.C;-;Feleletmentesítő,Dogahalasztó,Plusz;"../static/images/Balogh Anna.jpg"',
+    'Bors Henriette;211;11.A,12.A,9.KNY,12.D,11.D,10.D,13.D,13.E;"Veszteségeink érlelnek a győzelemre, csalódásaink a bölcsességre és a fájdalmaink a boldogságra";Feleletmentesítő;"../static/images/Bors Henriette.jpg"',
+    'Csókáné Judit;105;9.D,12.D,13.A,10.D,13.E,12.A;Örülök, hogy részese lehetek a Jedlik  szép eredményeinek, hogy jó értékrendű, tudásra vágyó tanulókkal dolgozok együtt, és segíthetem őket. Fejlődésük, aktivitásuk, bizalmuk motivál engem a fejlődésre, munkára.;Feleletmentesítő;"../static/images/Csókáné Judit.jpg"',
 ]
 
 const merch = [
@@ -25,3 +25,5 @@ export default {
     tanardata,
     merch,
 };
+
+//Örülök, hogy részese lehetek a Jedlik  szép eredményeinek, hogy jó értékrendű, tudásra vágyó tanulókkal dolgozok együtt, és segíthetem őket. Fejlődésük, aktivitásuk, bizalmuk motivál engem a fejlődésre, munkára.
